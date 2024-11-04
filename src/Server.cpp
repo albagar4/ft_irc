e5359d5615f31df37a566110ee743bc5ba4deed0
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-11-04 13:50:13 +0100

    added branch

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,15 +2,27 @@
 
 Server::Server(std::string port, std::string password): password(password) {
 	ft_print("IRC CONSTRUCTOR CALLED");
+
+  // Solo puertos numéricos
+  if (port.find_first_not_of("0123456789") != std::string::npos)
+    print_err("Port number is invalid");
 	this->port = atoi(port.c_str());
+
+  // Solo contraseñas alfanuméricas
 	for (unsigned long int i = 0; i < password.size(); i++) {
 		if (!isalnum(password[i]))
 		  print_err("Password only accepts alphanumeric characters");
 	}
+
+  // Crear el socket del servidor
 	this->serverSocket = socket(AF_INET, SOCK_STREAM, 0);
   if (this->serverSocket == -1)
     print_err("Unable to create server socket");
 	
+  int optval = 1;
+  if (setsockopt(this->serverSocket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1)
+    print_err("Unable to set options to server socket");
+
   // Especificaciones de la dirección
 	this->serverAddress.sin_family = AF_INET;
 	this->serverAddress.sin_port = htons(this->port);
