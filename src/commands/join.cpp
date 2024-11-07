321d8985f57973e85fe1e6260273226f626ade39
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-11-07 17:26:31 +0100

    carriage return and newline

--- src/commands/join.cpp
+++ src/commands/join.cpp
@@ -8,6 +8,7 @@ static std::string rpl_NamReply(Server server, Client client, Channel channel) {
     response += "= ";
     response += channel.getName() + " :";
     response += channel.getUserList();
+    response += "\r\n";
     return response;
 }
 static std::string rpl_EndOfNames(Server server, Client client, Channel channel) {
@@ -15,6 +16,7 @@ static std::string rpl_EndOfNames(Server server, Client client, Channel channel)
     response += client.getNick() + " ";
     response += channel.getName() + " :";
     response += "End of /NAMES list.";
+    response += "\r\n";
     return response;
 }
 static std::string rpl_Topic(Server server, Client client, Channel channel) {
@@ -22,6 +24,7 @@ static std::string rpl_Topic(Server server, Client client, Channel channel) {
     response += client.getNick() + " ";
     response += channel.getName() + " :";
     response += channel.getTopic();
+    response += "\r\n";
     return response;
 }
 static std::string rpl_Successful(Server server, Client client, Channel channel) {
@@ -29,14 +32,16 @@ static std::string rpl_Successful(Server server, Client client, Channel channel)
     response += " JOIN : ";
     response += channel.getName();
     if (channel.getTopic() != "") response += "\r\n" + rpl_Topic(server, client, channel);
-    response += "\r\n" + rpl_NamReply(server, client, channel);
-    response += "\r\n" + rpl_EndOfNames(server, client, channel) + "\r\n";
+    else response += "\r\n";
+    response += rpl_NamReply(server, client, channel);
+    response += rpl_EndOfNames(server, client, channel);
     return response;
 }
 static std::string err_NeedMoreParams(Server server, Client client) {
     std::string response = server.getHostname() + " 461 ";
     response += client.getNick();
     response += " JOIN :Not enough parameters";
+    response += "\r\n";
     return response;
 }
 static std::string err_ChannelIsFull(Server server, Client client, Channel channel) {
@@ -44,6 +49,7 @@ static std::string err_ChannelIsFull(Server server, Client client, Channel chann
     response += client.getNick() + " ";
     response += channel.getName();
     response += " :Cannot join channel (+l)";
+    response += "\r\n";
     return response;
 }
 // static std::string err_InviteOnlyChan(Server server, Client client, Channel channel) {
