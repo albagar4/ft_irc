e2021ee218c9db5f47315cc4927a21f0a7354210
Ester Castellano Rios <escastel@c2r8s6.42malaga.com>
2024-11-07 14:49:03 +0100

    falta comprobar si funciona correctamente

--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -3,7 +3,22 @@
 #include <ircserv.hpp>
 
 void Server::parseNick(std::string buffer, Client &client) {
-    (void)client;
     ft_print("Inside Nick: ");
     ft_print(buffer);
+    if (buffer.empty()){
+        print_err(this->getHostname() + " 431 :No nickname given");
+        return ;
+    }
+    if (buffer.find('#') >= 0 || buffer.find(':') >= 0|| buffer.find(' ') >= 0 || isdigit(buffer[0])){
+        print_err(this->getHostname() + " 432 " + buffer + " :Erroneus nickname");
+        return ;
+    }
+    for (int i = 0; i < this->getMap().size(); i++)
+    {
+        if (this->map[i].getNick().compare(buffer) == 0){
+            print_err(this->getHostname() + " 433 " + buffer + " :Nickname is already in use");
+            return ;
+        }
+    }
+    client.setNick(buffer);
 }
