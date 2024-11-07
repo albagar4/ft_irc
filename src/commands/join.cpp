80795d6a37e36853dbf2983f490a64ff03a9da1a
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-11-07 17:12:31 +0100

    join first pass done

--- src/commands/join.cpp
+++ src/commands/join.cpp
@@ -12,49 +12,61 @@ static std::string rpl_NamReply(Server server, Client client, Channel channel) {
 }
 static std::string rpl_EndOfNames(Server server, Client client, Channel channel) {
     std::string response = server.getHostname() + " 366 ";
-    response += client.getNick();
+    response += client.getNick() + " ";
     response += channel.getName() + " :";
     response += "End of /NAMES list.";
     return response;
 }
 static std::string rpl_Topic(Server server, Client client, Channel channel) {
     std::string response = server.getHostname() + " 332 ";
-    response += client.getNick();
+    response += client.getNick() + " ";
     response += channel.getName() + " :";
     response += channel.getTopic();
     return response;
 }
-static void rpl_Successful(Server server, Client client, Channel channel) {
+static std::string rpl_Successful(Server server, Client client, Channel channel) {
     std::string response = client.getHostname();
     response += " JOIN : ";
     response += channel.getName();
-
-    if (channel.getTopic() != "") response += "\n" + rpl_Topic(server, client, channel);
-    response += "\n" + rpl_NamReply(server, client, channel);
-    response += "\n" + rpl_EndOfNames(server, client, channel);
-    std::cout << "Reply would be:\n" << response << std::endl;
+    if (channel.getTopic() != "") response += "\r\n" + rpl_Topic(server, client, channel);
+    response += "\r\n" + rpl_NamReply(server, client, channel);
+    response += "\r\n" + rpl_EndOfNames(server, client, channel) + "\r\n";
+    return response;
 }
-
-static std::string err_NeedMoreParams(Server server, Client client) { ; }
-static std::string err_ChannelIsFull(Server server, Client client, Channel channel) { ; }
-
+static std::string err_NeedMoreParams(Server server, Client client) {
+    std::string response = server.getHostname() + " 461 ";
+    response += client.getNick();
+    response += " JOIN :Not enough parameters";
+    return response;
+}
+static std::string err_ChannelIsFull(Server server, Client client, Channel channel) {
+    std::string response = server.getHostname() + " 471 ";
+    response += client.getNick() + " ";
+    response += channel.getName();
+    response += " :Cannot join channel (+l)";
+    return response;
+}
+// static std::string err_InviteOnlyChan(Server server, Client client, Channel channel) {
+//     std::string response = server.getHostname() + " 473 ";
+//     response += client.getNick() + " ";
+//     response += channel.getName();
+//     response += " :Cannot join channel (+i)";
+//     return response;
+// }
 void Server::parseJoin(std::string buffer, Client &client) {
     Channel *temp;
+    client.setNick("alvega-g");
+    client.setUser("alvega-g");
+    client.setHostname();
     try {
         if (buffer.empty()) throw 461;
         std::vector<std::string> tokens = split(buffer, ',');
         for (size_t i = 0; i < tokens.size(); i++) {
-            if (tokens.size() > 1 && tokens[i].find_first_of(":") != std::string::npos) {
-                ft_print(
-                    "Joining multiple channels and setting topic in the same command is not "
-                    "supported",
-                    RED);
-                return;
-            }
-            std::string topic =
-                tokens[i].substr(tokens[i].find_first_of(":") + 1, tokens[i].size());
+            size_t colonPos = tokens[i].find_first_of(":");
+            if (tokens.size() > 1 && colonPos != std::string::npos) throw 461;
+            std::string topic = tokens[i].substr(colonPos + 1, tokens[i].size());
             if (topic != tokens[i]) {
-                tokens[i] = tokens[i].substr(0, tokens[i].find_first_of(":") - 1);
+                tokens[i] = tokens[i].substr(0, colonPos - 1);
             } else {
                 topic = "";
             }
@@ -64,19 +76,20 @@ void Server::parseJoin(std::string buffer, Client &client) {
                 this->channels.push_back(newChannel);
             } else {
                 temp = this->findChannel(tokens[i]);
-                if (temp->getUserLimit() == temp->getClients().size()) throw 471;
+                if ((size_t)temp->getUserLimit() == temp->getClients().size()) throw 471;
                 // Check if user is invited: if (temp->getInviteOnly() == true && ) throw 473;
                 this->findChannel(tokens[i])->addClient(client);
             }
-            rpl_Successful(*this, client, *this->findChannel(tokens[i]));
+            client.setResponse(rpl_Successful(*this, client, *this->findChannel(tokens[i])));
         }
     } catch (int code) {
         if (code == 461)
-            err_NeedMoreParams(*this, client);
+            client.setResponse(err_NeedMoreParams(*this, client));
         else if (code == 471)
-            err_ChannelIsFull(*this, client, temp);
+            client.setResponse(err_ChannelIsFull(*this, client, *temp));
         // else if (code == 473)
-        //  err_InviteOnlyChan(*this, client, temp);
+        //  client.setResponse(err_InviteOnlyChan(*this, client, temp));
     }
+    std::cout << client.getResponse() << std::endl;
     printChannels();
 }
