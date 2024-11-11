d515f257be6c5148b5ffa7ef98af5f4dc95bd192
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-11-11 15:42:19 +0100

    added quit

--- src/commands/quit.cpp
+++ src/commands/quit.cpp
@@ -3,7 +3,6 @@
 #include <ircserv.hpp>
 
 void Server::parseQuit(std::string buffer, Client &client) {
-    (void)client;
-    ft_print("Inside Quit: ");
-    ft_print(buffer);
+    std::string reason = buffer;
+    this->disconnectClient(client);
 }
