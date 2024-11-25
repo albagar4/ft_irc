cfa446c7f876c62f0a440e806babcad27547d4c2
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-11-25 16:36:23 +0100

    added <C-c> control for clean shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <Server.hpp>
 #include <ircserv.hpp>
 
+int g_signal;
+
+void signalHandler(int signum) { g_signal = signum; }
+
 int main(int argc, char **argv) {
     if (argc != 3)
         print_err("Incorrect nbr of parameters\nUsage: ./ircserv <port> <password>");
     else {
+        signal(SIGINT, signalHandler);
         Server server(argv[1], argv[2]);
         initializeErrorMessages();
-        while (true) {
+        while (!g_signal) {
             if (server.checkConnections() > 0) {
                 server.iterateFds();
-                // parseResponse(int i)
-                // executeResponse(int i)
             }
         }
     }
