e6c6577cd45fcebe1672b75de853a7f2e597fda4
Álvaro Vega Gómez <alvarvg@gmail.com>
2024-12-03 13:49:56 +0100

    fixed still reachable when invalid input

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,10 @@ int g_signal = 0;
 void signalHandler(int signum) { g_signal = signum; }
 
 int main(int argc, char **argv) {
-    if (argc != 3)
-        print_err("Incorrect nbr of parameters\nUsage: ./ircserv <port> <password>");
-    else {
+    if (argc != 3) {
+        ft_print("Incorrect nbr of parameters\nUsage: ./ircserv <port> <password>", RED);
+        return 1;
+    } else {
         signal(SIGINT, signalHandler);
         signal(SIGQUIT, signalHandler);
         Server server(argv[1], argv[2]);
